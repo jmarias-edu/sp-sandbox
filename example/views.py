db8884a077804886ee5fe1f6c10d637d374ec69f
jmarias-edu <jmarias@up.edu.ph>
2023-11-26 17:56:22 +0800

    feat: GIVE example test

--- example/views.py
+++ example/views.py
@@ -3,14 +3,17 @@ from datetime import datetime
 
 from django.http import HttpResponse
 
+# def index(request):
+#     now = datetime.now()
+#     html = f'''
+#     <html>
+#         <body>
+#             <h1>Hello from Vercel!</h1>
+#             <p>The current time is { now }.</p>
+#         </body>
+#     </html>
+#     '''
+#     return HttpResponse(html)
+
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    return render(request, "example/index.html")
